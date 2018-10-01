e1c9c2459d2ccf3f81ee21fcfcfa540636a6c712
Lorenzo Ambrosini <lorenzo.ambrosini@mail.polimi.it>
2018-10-01 13:19:52 +0200

    Fiamma

--- sketch.js
+++ sketch.js
@@ -5,14 +5,70 @@ function preload(){
 function setup() {
    createCanvas(windowWidth, windowHeight);
    angleMode(DEGREES);
-   frameRate(20);
+   frameRate(60);
    background(30);
+   strokeWeight(2);
+
+   noStroke();
+
+
 }
 
 function draw() {
 
-  stroke(256);
+  var x;
+  var y;
 
-  translate(width/2, height/2);
-  line(100, 0, cos(frameCount*5) * 100, sin(frameCount*5) * 100);
+  translate(width/2, height/2 + 200);
+  stroke(lerpColor(color('#ba1500'), color('#ffe41c'), frameCount/700));
+
+  x = sin(frameCount*5) * 100;
+  y = cos(frameCount*5) * 100;
+
+  if(frameCount > 72){
+    x = sin(frameCount*5) * 150;
+    y = cos(frameCount*5) * 100 - 25;
+  }
+
+  if(frameCount > 144){
+    x = sin(frameCount*5) * 200;
+    y = cos(frameCount*5) * 100 - 50;
+  }
+
+  if(frameCount > 216){
+    x = sin(frameCount*5) * 195;
+    y = cos(frameCount*5) * 100 - 75;
+  }
+
+  if(frameCount > 288){
+    x = sin(frameCount*5) * 185;
+    y = cos(frameCount*5) * 100 - 110;
+  }
+
+  if(frameCount > 360){
+    x = sin(frameCount*5) * 170;
+    y = cos(frameCount*5) * 100 - 150;
+  }
+
+  if(frameCount > 432){
+    x = sin(frameCount*5) * 140;
+    y = cos(frameCount*5) * 100 - 210;
+  }
+
+  if(frameCount > 504){
+    x = sin(frameCount*5) * 100;
+    y = cos(frameCount*5) * 100 - 300;
+  }
+
+  if(frameCount > 576){
+    x = sin(frameCount*5) * 50;
+    y = cos(frameCount*5) * 100 - 400;
+  }
+
+  if(frameCount >= 648){
+    noLoop();
+  }
+
+  line(0, 100, x, y);
+
 }
